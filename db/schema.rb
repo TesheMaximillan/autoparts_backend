64ed3d8ff10492546c087dfa4960a1ee66cec9e4
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-11 16:16:48 +0300

    Schema file is updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_064055) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_130002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "name", null: false
+    t.string "part_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "products_purchases", id: false, force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "purchase_id", null: false
+  end
+
+  create_table "products_sales", id: false, force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "sale_id", null: false
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.date "date"
+    t.string "vendor_name"
+    t.string "reference_number"
+    t.integer "quantity"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.date "date"
+    t.string "customer_name"
+    t.string "reference_number"
+    t.integer "quantity"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -26,4 +71,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_064055) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
