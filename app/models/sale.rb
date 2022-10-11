b38670c0c616ae694287314074d82e57f54a8bbc
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-11 22:00:04 +0300

    Modify date validation

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,7 +3,7 @@ class Sale < ApplicationRecord
   belongs_to :user
 
   validates :vendor_name, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :date, presence: true, date: { before_or_equal_to: Date.today }
+  validates :date, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than: 0 }
 end
