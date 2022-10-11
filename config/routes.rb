42595fe1bd37693aa8300cdeba8e0401b4fab8cb
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-11 22:00:36 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :categories do
+        resources :products, only: [:index, :create, :update, :destroy]
+      end
+      resources :sales, only: [:index, :create, :show, :update, :destroy]
+      resources :purchases, only: [:index, :create, :show, :update, :destroy]
       resources :sessions, only: [:create]
       resources :registrations, only: [:create]
       delete :logout, to: "sessions#logout"
