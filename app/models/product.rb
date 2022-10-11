3adf4685a03b44c9cee04240d2e383e675c00f3e
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-11 16:13:48 +0300

    Create product model

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ApplicationRecord
+  belongs_to :category
+  has_and_belongs_to_many :purchases, dependent: :destroy, join_table: :purchases_products
+  has_and_belongs_to_many :sales, dependent: :destroy, join_table: :sales_products
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :part_number, presence: true, length: { minimum: 3, maximum: 50 }
+end
