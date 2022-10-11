8254a646c3d930caac3d429792f6aeaf7c7cb157
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-11 16:14:35 +0300

    Create purchase model

---
+++ db/migrate/20221007105612_create_purchases.rb
@@ -0,0 +1,13 @@
+class CreatePurchases < ActiveRecord::Migration[7.0]
+  def change
+    create_table :purchases do |t|
+      t.date :date
+      t.string :vendor_name
+      t.string :reference_number
+      t.integer :quantity
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
