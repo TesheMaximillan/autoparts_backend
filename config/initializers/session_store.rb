b6013d40d1ff2effca2a85efe0bdf8b0d2b65728
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-11 22:01:21 +0300

    Modify  session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, secure: true
+Rails.application.config.session_store :cookie_store, key: "_auto_parts_app"
