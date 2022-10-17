b51d694549a5a1c0784933d1a9c1da02e691531e
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-17 23:33:54 +0300

    Create vendor table

---
+++ app/models/vendor.rb
@@ -0,0 +1,8 @@
+class Vendor < ApplicationRecord
+  belongs_to :user
+  has_many :purchase_transactions, dependent: :destroy
+  has_many :products, through: :purchase_transactions
+  has_many :purchases, through: :purchase_transactions
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+end
