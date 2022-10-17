6f3e54b5484bf2cc193217804a2ab07f01476386
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-17 23:34:35 +0300

    Remove product_sale and product_purchase join tables

--- db/migrate/20221011125906_product_sales.rb
+++
@@ -1,5 +0,0 @@
-class ProductSales < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :products, :sales
-  end
-end
--- db/migrate/20221011130002_product_purchase.rb
+++
@@ -1,5 +0,0 @@
-class ProductPurchase < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :products, :purchases
-  end
-end
