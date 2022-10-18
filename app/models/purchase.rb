99bb4dbcb7bddfda96a91c61c9d467c7b52c3565
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-18 19:02:35 +0300

    Modify purchase model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,8 +4,10 @@ class Purchase < ApplicationRecord
   has_many :products, through: :purchase_transactions
   has_many :vendors, through: :purchase_transactions
 
-  validates :vendor_name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :date, presence: true
+  validates :received_by, length: { minimum: 3, maximum: 50 }
+  validates :reference_number, presence: true, length: { minimum: 3, maximum: 50 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :status, presence: true, length: { minimum: 3, maximum: 50 }
 end
