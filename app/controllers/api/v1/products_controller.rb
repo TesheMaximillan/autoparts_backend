e0f9959703412fa0d469b2aa343cbe6b5e8e3e00
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-02 12:30:46 +0300

    Create product controller methods

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,47 +1,76 @@
 class Api::V1::ProductsController < ApplicationController
-  include CurrentUserConcern
-
+  before_action :set_product, only: %i[show update destroy]
+  before_action :ret_products, only: %i[index update]
   def index
-    @products = Product.all
-    render json: @products
+    @products = @products.map do |product|
+      quantity = StockProduct.where(product_id: product.id).pluck(:quantity).sum
+      next unless quantity.positive?
+
+      {
+        id: product.id,
+        name: product.name,
+        part_number: product.part_number,
+        brand: product.brand,
+        status: product.status,
+        selling: product.selling,
+        cost: product.cost,
+        category_id: product.category_id,
+        quantity:
+      }
+    end
+
+    render json: @products.compact
   end
 
   def create
     @product = Product.new(product_params)
-    @product.user = @current_user
 
     if @product.save
+      StockProduct.create(stock_id: params[:stock_id], product_id: @product.id, quantity: params[:quantity])
       render json: @product, status: :created
     else
-      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @product.errors.messages }, status: :unprocessable_entity
     end
   end
 
   def show
-    @product = Product.find(params[:id])
     render json: @product
   end
 
   def update
-    @product = Product.find(params[:id])
     @product.update(product_params)
+    @stock_transaction = StockProduct.where(stock_id: params[:stock_id], product_id: @product.id).first
 
     if @product.valid?
+      if @stock_transaction
+        @stock_transaction.update(quantity: params[:quantity])
+      else
+        StockProduct.create(stock_id: params[:stock_id], product_id: @product.id, quantity: params[:quantity])
+      end
       render json: @product, status: :created
     else
-      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @product.errors.messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    render json: @product
+    product_id = params[:id].split(',')[0]
+    stock_id = params[:id].split(',')[1]
+    StockProduct.where(stock_id:, product_id:).first.update(quantity: 0)
+    render json: { message: 'Product deleted successfully!!!' }
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:user_id, :name, :part_number, :category_id, :stock_id, :cost, :selling, :quantity)
+    params.require(:product).permit(:name, :part_number, :brand, :status, :selling, :cost, :category_id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def ret_products
+    @products = Product.all
   end
 end
