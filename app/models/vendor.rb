5c5380b7b76b52a017271aca944d0ba4d6430f32
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-02 12:36:57 +0300

    Add lowercase method

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,8 +1,14 @@
 class Vendor < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :purchase_transactions, dependent: :destroy
   has_many :products, through: :purchase_transactions
   has_many :purchases, through: :purchase_transactions
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+
+  before_save :downcase_field
+
+  def downcase_field
+    name.downcase!
+  end
 end
