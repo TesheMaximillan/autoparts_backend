3368e6730e802a58bd8ac2d59df97955e54c3ee7
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-02 12:28:07 +0300

    fix migration

--- db/migrate/20221007105038_create_sales.rb
+++ db/migrate/20221007105038_create_sales.rb
@@ -1,7 +1,6 @@
 class CreateSale < ActiveRecord::Migration[7.0]
   def change
     create_table :sale do |t|
-      t.references :user, null: false, foreign_key: true
       t.date :date
       t.string :received_by
       t.string :reference_number
--- db/migrate/20221018162241_transfer_transaction.rb
+++
@@ -1,5 +0,0 @@
-class TransferTransaction < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :transfers, :products
-  end
-end
