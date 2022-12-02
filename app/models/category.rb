beeac62230b1a0e6ad915eb8835f7c24ff1496bb
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-02 12:34:18 +0300

    Add lower case method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
   has_many :products
-  belongs_to :user
+  # belongs_to :user
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+
+  before_save :downcase_field
+
+  def downcase_field
+    name.downcase!
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,14 @@
 class Customer < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :sale_transactions, dependent: :destroy
   has_many :products, through: :sale_transactions
   has_many :sales, through: :sale_transactions
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+
+  before_save :downcase_field
+
+  def downcase_field
+    name.downcase!
+  end
 end
