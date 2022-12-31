a3af4fcf06ad4eda98facd83f2db6e03320cce7a
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-31 17:11:47 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Seeding Start
+
+puts "[db/seeds.rb] Seeding data..."
+User.create!([
+               {username: "teshe", full_name: "Teshome Kurabachew", email: "teshe@gmail.com", password: "password", password_confirmation: "password"},
+               {username: "sinte", full_name: "Sintayehu", email: "sinte@gmail.com", password: "password", password_confirmation: "password"}
+             ])
+
+Stock.create!([
+                {name: "Shop"},
+                {name: "Store"}
+              ])
+
+Category.create!([
+                   {name: "Fuel Filter"},
+                   {name: "Oil Filter"}
+                 ])
+
+Customer.create!([
+                   {name: "customer1"},
+                   {name: "customer2"}
+                 ])
+
+Vendor.create!([
+                 {name: "vendor1"},
+                 {name: "vendor2"}
+               ])
+
+puts "[db/seeds.rb] Seeding data...done"
